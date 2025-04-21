ab0c69dc9e5c26ef30b6b8579cd34b29f7b76cb5
JeongSangCheol1029 <jsc3192@naver.com>
2025-04-21 16:10:24 +0900

    d

--- 404ERROR/app/main/__init__.py
+++ 404ERROR/app/main/__init__.py
@@ -1,3 +1,2 @@
 # app/main/__init__.py
 from app.main.blueprint import main_bp
-
--- 404ERROR/run.py
+++ 404ERROR/run.py
@@ -8,5 +8,6 @@ app, db = create_app()
 app.register_blueprint(auth_bp)
 app.register_blueprint(main_bp)
 
+
 if __name__ == "__main__":
     app.run(debug=True)
