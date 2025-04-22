5ca714f695ada1e1e786577f402ee1bc6cd81898
HyeongseopSo0914 <shs1492@gmail.com>
2025-04-22 17:06:33 +0900

    run.py 변경

--- 404ERROR/run.py
+++ 404ERROR/run.py
@@ -9,4 +9,6 @@ app.register_blueprint(auth_bp)
 app.register_blueprint(main_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
